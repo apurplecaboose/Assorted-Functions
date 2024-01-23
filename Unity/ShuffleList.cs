05514b0fba59f9d794e19315a4a0a2b0fa6de98d
apurplecaboose <38011964+apurplecaboose@users.noreply.github.com>
2024-01-23 10:12:07 +0000

    Create ShuffleList.cs

---
+++ Unity/ShuffleList.cs
@@ -0,0 +1,14 @@
+    /// <summary>
+    /// Input type list and return type list
+    /// </summary>
+List<int> Shuffle(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int temp = list[i];
+            int randomIndex = UnityEngine.Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+        return list;
+    }
